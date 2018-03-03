4067862facb77d04346cedf4af7e0b760d00b0ce
Vehrn <amb059@email.uark.edu>
2018-03-02 18:37:57 -0600

    Initial UI for Create Employee Page (needs DataBase interaction)

--- app/src/main/java/edu/uark/uarkregisterapp/LandingActivity.java
+++ app/src/main/java/edu/uark/uarkregisterapp/LandingActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import edu.uark.uarkregisterapp.models.api.EmployeeCount;
 import edu.uark.uarkregisterapp.models.transition.ProductTransition;
 
 public class LandingActivity extends AppCompatActivity {
@@ -29,5 +30,18 @@ public class LandingActivity extends AppCompatActivity {
 		);
 
 		this.startActivity(intent);
+	}
+    /*(Austin): Uncertain of how to check total employee count (value stored in server)
+    putting a note in the GroupMe.
+     */
+	public void goToEmployeeLoginOnClick(View view) {
+	    EmployeeCount temp;
+        temp = new EmployeeCount();
+        if(temp.getCount() <= 0) {
+            this.startActivity(new Intent(getApplicationContext(), CreateEmployeeScreen.class));
+        }
+	    else {
+            this.startActivity(new Intent(getApplicationContext(), EmployeeLoginActivity.class));
+        }
 	}
 }

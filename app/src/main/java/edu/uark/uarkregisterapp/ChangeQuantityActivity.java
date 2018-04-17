a1fd45d88fb8f7f29d6d75b3bae6a9854b987fb6
rschuljak <35670717+rschuljak@users.noreply.github.com>
2018-04-17 08:52:36 -0500

    Created Change Quanity Activity and fixed a few bugs

--- app/src/main/java/edu/uark/uarkregisterapp/ChangeQuantityActivity.java
+++ app/src/main/java/edu/uark/uarkregisterapp/ChangeQuantityActivity.java
@@ -2,6 +2,7 @@ package edu.uark.uarkregisterapp;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.widget.TextView;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,23 +19,55 @@ import edu.uark.uarkregisterapp.models.transition.ProductTransition;
 
 public class ChangeQuantityActivity extends AppCompatActivity {
 
+    private ProductTransition productTransition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_quantity);
+        this.productTransition = this.getIntent().getParcelableExtra("intent_extra_product");
+    }
+    @Override
+    protected void onResume(){
+        //print the product lookup code on top of acitivty
+        super.onResume();
+        this.setProductIDTextView().setText(this.productTransition.getLookupCode());
     }
 
     public void saveButtonOnClick(View view) {
-        if (!this.validateInput())
+        if (this.validateInput())
         {
+            this.productTransition.setCount(Integer.parseInt(this.setProductCountEditText().getText().toString()));
+            Intent intent = new Intent(this.getApplicationContext(), ShoppingCartActivity.class);
+            //intent.putExtra("intent_extra_employee", this.productTransition);
+            this.startActivity(intent);
             return;
         }
-        //This is your job russel, make a save button and have it add items to cart
+        else{
+            String validationMessage = this.getString(R.string.validation_product_count);
+                new AlertDialog.Builder(this).
+                        setMessage(validationMessage).
+                        setPositiveButton(
+                                R.string.button_dismiss,
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.dismiss();
+                                    }
+                                }
+                        ).
+                        create().
+                        show();
+            }
+
+        //This is your job russel(Cmon Austin you know you how to spell my name Russell), make a save button and have it add items to cart
         //I have done the rest for you. The arrayList is in ShoppingCartActivity.
     }
 
     private EditText setProductCountEditText() {
         return (EditText) this.findViewById(R.id.edit_text_set_product_count);
+    }
+
+    private TextView setProductIDTextView(){
+        return (TextView) this.findViewById(R.id.text_view_product_lookup_code);
     }
 
     private boolean validateInput() {
